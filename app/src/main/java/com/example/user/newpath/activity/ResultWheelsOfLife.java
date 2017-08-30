cd072b8e1df5e38c0dac05c0dd45265b4647059d
Gabriel Marcos <gabrielmareia@hotmail.com>
2017-08-30 20:07:53 -0300

    Add icons

--- app/src/main/java/com/example/user/newpath/activity/ResultWheelsOfLife.java
+++ app/src/main/java/com/example/user/newpath/activity/ResultWheelsOfLife.java
@@ -1,15 +1,32 @@
 package com.example.user.newpath.activity;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.user.newpath.R;
+import com.example.user.newpath.fragment.DashboardUser;
+import com.example.user.newpath.fragment.UserProfile;
 
 public class ResultWheelsOfLife extends AppCompatActivity {
-
+    private Button btnFinalizar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_wheels_of_life);
+        btnFinalizar = (Button) findViewById(R.id.btnFinalizar);
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startDashboard();
+            }
+        });
+    }
+    public void startDashboard() {
+        Intent intent = new Intent(ResultWheelsOfLife.this, UserProfile.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/user/newpath/fragment/DashboardUser.java
+++ app/src/main/java/com/example/user/newpath/fragment/DashboardUser.java
@@ -7,16 +7,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.example.user.newpath.R;
 
 public class DashboardUser extends Fragment {
-
+    protected View view;
+    private LinearLayout box_finalizar_desafio;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-
-        return inflater.inflate(R.layout.fragment_dashboard_user, container, false);
+        view = inflater.inflate(R.layout.fragment_dashboard_user, container, false);
+        initViews(view);
+        return view;
     }
+
+    protected void initViews(View view){
+
+
+    }
+    private View.OnClickListener click_finalizar = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
 }
