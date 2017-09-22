36c1afbeef4bbe86470fcd31687b8a8ee6f93ff7
Gabriel Marcos <gabriel.marcos@jussi.com.br>
2017-09-21 21:36:18 -0300

    Prepare get with retrofit

---
+++ app/src/main/java/com/example/user/newpath/model/ItensChallenge.java
@@ -0,0 +1,8 @@
+package com.example.user.newpath.model;
+
+/**
+ * Created by jussi on 9/21/17.
+ */
+
+public class ItensChallenge {
+}
---
+++ app/src/main/java/com/example/user/newpath/request/RequestChallenge.java
@@ -0,0 +1,8 @@
+package com.example.user.newpath.request;
+
+/**
+ * Created by jussi on 9/21/17.
+ */
+
+public interface RequestChallenge {
+}
